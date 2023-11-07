7d9c545e235a7794de7d41ac45c7e3782750de54
OHMORIYUSUKE <ohmoriyusuke@gmail.com>
2023-11-07 23:22:56 +0900

    first commit

---
+++ src/anime_character_birthday_feed/anime_character_birthday_feed/idol_images.py
@@ -0,0 +1,242 @@
+idol_images = [
+    {
+        "name": "春香",
+        "url": "https://raw.githubusercontent.com/tankarup/ML-4koma-viewer/main/icons/haruka.jpg",
+    },
+    {
+        "name": "千早",
+        "url": "https://raw.githubusercontent.com/tankarup/ML-4koma-viewer/main/icons/chihaya.jpg",
+    },
+    {
+        "name": "美希",
+        "url": "https://raw.githubusercontent.com/tankarup/ML-4koma-viewer/main/icons/miki.jpg",
+    },
+    {
+        "name": "雪歩",
+        "url": "https://raw.githubusercontent.com/tankarup/ML-4koma-viewer/main/icons/yukiho.jpg",
+    },
+    {
+        "name": "やよい",
+        "url": "https://raw.githubusercontent.com/tankarup/ML-4koma-viewer/main/icons/yayoi.jpg",
+    },
+    {
+        "name": "真",
+        "url": "https://raw.githubusercontent.com/tankarup/ML-4koma-viewer/main/icons/makoto.jpg",
+    },
+    {
+        "name": "伊織",
+        "url": "https://raw.githubusercontent.com/tankarup/ML-4koma-viewer/main/icons/iori.jpg",
+    },
+    {
+        "name": "貴音",
+        "url": "https://raw.githubusercontent.com/tankarup/ML-4koma-viewer/main/icons/takane.jpg",
+    },
+    {
+        "name": "律子",
+        "url": "https://raw.githubusercontent.com/tankarup/ML-4koma-viewer/main/icons/ritsuko.jpg",
+    },
+    {
+        "name": "あずさ",
+        "url": "https://raw.githubusercontent.com/tankarup/ML-4koma-viewer/main/icons/azusa.jpg",
+    },
+    {
+        "name": "亜美",
+        "url": "https://raw.githubusercontent.com/tankarup/ML-4koma-viewer/main/icons/ami.jpg",
+    },
+    {
+        "name": "真美",
+        "url": "https://raw.githubusercontent.com/tankarup/ML-4koma-viewer/main/icons/mami.jpg",
+    },
+    {
+        "name": "響",
+        "url": "https://raw.githubusercontent.com/tankarup/ML-4koma-viewer/main/icons/hibiki.jpg",
+    },
+    {
+        "name": "未来",
+        "url": "https://raw.githubusercontent.com/tankarup/ML-4koma-viewer/main/icons/mirai.jpg",
+    },
+    {
+        "name": "静香",
+        "url": "https://raw.githubusercontent.com/tankarup/ML-4koma-viewer/main/icons/shizuka.jpg",
+    },
+    {
+        "name": "翼",
+        "url": "https://raw.githubusercontent.com/tankarup/ML-4koma-viewer/main/icons/tsubasa.jpg",
+    },
+    {
+        "name": "琴葉",
+        "url": "https://raw.githubusercontent.com/tankarup/ML-4koma-viewer/main/icons/kotoha.jpg",
+    },
+    {
+        "name": "エレナ",
+        "url": "https://raw.githubusercontent.com/tankarup/ML-4koma-viewer/main/icons/elena.jpg",
+    },
+    {
+        "name": "美奈子",
+        "url": "https://raw.githubusercontent.com/tankarup/ML-4koma-viewer/main/icons/minako.jpg",
+    },
+    {
+        "name": "恵美",
+        "url": "https://raw.githubusercontent.com/tankarup/ML-4koma-viewer/main/icons/megumi.jpg",
+    },
+    {
+        "name": "まつり",
+        "url": "https://raw.githubusercontent.com/tankarup/ML-4koma-viewer/main/icons/matsuri.jpg",
+    },
+    {
+        "name": "星梨花",
+        "url": "https://raw.githubusercontent.com/tankarup/ML-4koma-viewer/main/icons/serika.jpg",
+    },
+    {
+        "name": "茜",
+        "url": "https://raw.githubusercontent.com/tankarup/ML-4koma-viewer/main/icons/akane.jpg",
+    },
+    {
+        "name": "杏奈",
+        "url": "https://raw.githubusercontent.com/tankarup/ML-4koma-viewer/main/icons/anna.jpg",
+    },
+    {
+        "name": "ロコ",
+        "url": "https://raw.githubusercontent.com/tankarup/ML-4koma-viewer/main/icons/roco.jpg",
+    },
+    {
+        "name": "百合子",
+        "url": "https://raw.githubusercontent.com/tankarup/ML-4koma-viewer/main/icons/yuriko.jpg",
+    },
+    {
+        "name": "紗代子",
+        "url": "https://raw.githubusercontent.com/tankarup/ML-4koma-viewer/main/icons/sayoko.jpg",
+    },
+    {
+        "name": "亜利沙",
+        "url": "https://raw.githubusercontent.com/tankarup/ML-4koma-viewer/main/icons/arisa.jpg",
+    },
+    {
+        "name": "海美",
+        "url": "https://raw.githubusercontent.com/tankarup/ML-4koma-viewer/main/icons/umi.jpg",
+    },
+    {
+        "name": "育",
+        "url": "https://raw.githubusercontent.com/tankarup/ML-4koma-viewer/main/icons/iku.jpg",
+    },
+    {
+        "name": "朋花",
+        "url": "https://raw.githubusercontent.com/tankarup/ML-4koma-viewer/main/icons/tomoka.jpg",
+    },
+    {
+        "name": "エミリー",
+        "url": "https://raw.githubusercontent.com/tankarup/ML-4koma-viewer/main/icons/emily.jpg",
+    },
+    {
+        "name": "志保",
+        "url": "https://raw.githubusercontent.com/tankarup/ML-4koma-viewer/main/icons/shiho.jpg",
+    },
+    {
+        "name": "歩",
+        "url": "https://raw.githubusercontent.com/tankarup/ML-4koma-viewer/main/icons/ayumu.jpg",
+    },
+    {
+        "name": "ひなた",
+        "url": "https://raw.githubusercontent.com/tankarup/ML-4koma-viewer/main/icons/hinata.jpg",
+    },
+    {
+        "name": "可奈",
+        "url": "https://raw.githubusercontent.com/tankarup/ML-4koma-viewer/main/icons/kana.jpg",
+    },
+    {
+        "name": "奈緒",
+        "url": "https://raw.githubusercontent.com/tankarup/ML-4koma-viewer/main/icons/nao.jpg",
+    },
+    {
+        "name": "千鶴",
+        "url": "https://raw.githubusercontent.com/tankarup/ML-4koma-viewer/main/icons/chizuru.jpg",
+    },
+    {
+        "name": "このみ",
+        "url": "https://raw.githubusercontent.com/tankarup/ML-4koma-viewer/main/icons/konomi.jpg",
+    },
+    {
+        "name": "環",
+        "url": "https://raw.githubusercontent.com/tankarup/ML-4koma-viewer/main/icons/tamaki.jpg",
+    },
+    {
+        "name": "風花",
+        "url": "https://raw.githubusercontent.com/tankarup/ML-4koma-viewer/main/icons/fuka.jpg",
+    },
+    {
+        "name": "美也",
+        "url": "https://raw.githubusercontent.com/tankarup/ML-4koma-viewer/main/icons/miya.jpg",
+    },
+    {
+        "name": "のり子",
+        "url": "https://raw.githubusercontent.com/tankarup/ML-4koma-viewer/main/icons/noriko.jpg",
+    },
+    {
+        "name": "瑞希",
+        "url": "https://raw.githubusercontent.com/tankarup/ML-4koma-viewer/main/icons/mizuki.jpg",
+    },
+    {
+        "name": "可憐",
+        "url": "https://raw.githubusercontent.com/tankarup/ML-4koma-viewer/main/icons/karen.jpg",
+    },
+    {
+        "name": "莉緒",
+        "url": "https://raw.githubusercontent.com/tankarup/ML-4koma-viewer/main/icons/rio.jpg",
+    },
+    {
+        "name": "昴",
+        "url": "https://raw.githubusercontent.com/tankarup/ML-4koma-viewer/main/icons/subaru.jpg",
+    },
+    {
+        "name": "麗花",
+        "url": "https://raw.githubusercontent.com/tankarup/ML-4koma-viewer/main/icons/reika.jpg",
+    },
+    {
+        "name": "桃子",
+        "url": "https://raw.githubusercontent.com/tankarup/ML-4koma-viewer/main/icons/momoko.jpg",
+    },
+    {
+        "name": "ジュリア",
+        "url": "https://raw.githubusercontent.com/tankarup/ML-4koma-viewer/main/icons/julia.jpg",
+    },
+    {
+        "name": "紬",
+        "url": "https://raw.githubusercontent.com/tankarup/ML-4koma-viewer/main/icons/tsumugi.jpg",
+    },
+    {
+        "name": "歌織",
+        "url": "https://raw.githubusercontent.com/tankarup/ML-4koma-viewer/main/icons/kaori.jpg",
+    },
+    {
+        "name": "小鳥",
+        "url": "https://raw.githubusercontent.com/tankarup/ML-4koma-viewer/main/icons/kotori.jpg",
+    },
+    {
+        "name": "美咲",
+        "url": "https://raw.githubusercontent.com/tankarup/ML-4koma-viewer/main/icons/misaki.jpg",
+    },
+    {
+        "name": "プロデューサー",
+        "url": "https://raw.githubusercontent.com/tankarup/ML-4koma-viewer/main/icons/producer.jpg",
+    },
+    {
+        "name": "社長",
+        "url": "https://raw.githubusercontent.com/tankarup/ML-4koma-viewer/main/icons/takagi.jpg",
+    },
+    {
+        "name": "そら",
+        "url": "https://raw.githubusercontent.com/tankarup/ML-4koma-viewer/main/icons/sora.jpg",
+    },
+    {
+        "name": "詩花",
+        "url": "https://raw.githubusercontent.com/tankarup/ML-4koma-viewer/main/icons/sika.jpg",
+    },
+    {
+        "name": "玲音",
+        "url": "https://raw.githubusercontent.com/tankarup/ML-4koma-viewer/main/icons/leon.jpg",
+    },
+    {
+        "name": "黒井社長",
+        "url": "https://raw.githubusercontent.com/tankarup/ML-4koma-viewer/main/icons/kuroi.jpg",
+    },
+]
---
+++ src/anime_character_birthday_feed/anime_character_birthday_feed/main.py
@@ -0,0 +1,43 @@
+#! /usr/bin/env python
+import feedgenerator
+import requests
+import json
+import datetime
+from anime_character_birthday_feed.idol_images import idol_images
+
+today = datetime.date.today()
+# today = datetime.datetime(2017, 4, 3, 9, 55, 28)
+
+feed = feedgenerator.Rss201rev2Feed(
+    title="本日、誕生日のアイドルマスター ミリオンライブ！のアイドルを紹介🎂",
+    link="https://millionlive-anime.idolmaster-official.jp/",
+    description="本日" + str(today.month) + "月" + str(today.day) + "日が誕生日のアイドルは...",
+    language="ja",
+)
+
+response = requests.get("https://api.matsurihi.me/api/mltd/v2/idols")
+idols = json.loads(response.text)
+
+for idol in idols:
+    if (
+        idol["birthday"]["month"] == today.month
+        and idol["birthday"]["day"] == today.day
+    ):
+        for image in idol_images:
+            if image["name"] in idol["fullName"]:
+                feed.add_item(
+                    title=idol["fullName"] + "（" + idol["fullNameRuby"] + "）",
+                    link=image["url"],
+                    description=idol["firstName"]
+                    + "ちゃんは、"
+                    + idol["birthplace"]["name"]
+                    + "出身で、趣味は"
+                    + idol["hobby"]
+                    + "、好きなことは"
+                    + idol["favorites"]
+                    + "✨",
+                    pubdate=today,
+                )
+
+with open("test.rss", "w") as fp:
+    feed.write(fp, "utf-8")
